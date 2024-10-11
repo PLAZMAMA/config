f00c161ca3de1f4c5f5eab3561e7fca39eb8ac2a
PLAZMAMA <maorioriori@gmail.com>
2024-10-10 23:50:01 -0400

    add tabs navigation keybindings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,5 +7,19 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Tokyo Night Moon'
 config.window_background_opacity = 0.8
 
+local act = wezterm.action
+config.keys = {
+    { key = '1', mods = 'ALT', action = act.ActivateTab(0) },
+    { key = '2', mods = 'ALT', action = act.ActivateTab(1) },
+    { key = '3', mods = 'ALT', action = act.ActivateTab(2) },
+    { key = '4', mods = 'ALT', action = act.ActivateTab(3) },
+    { key = '5', mods = 'ALT', action = act.ActivateTab(4) },
+    { key = '5', mods = 'SHIFT|ALT|CTRL', action = act.SplitHorizontal{ domain = 'CurrentPaneDomain' } },
+    { key = '6', mods = 'ALT', action = act.ActivateTab(5) },
+    { key = '7', mods = 'ALT', action = act.ActivateTab(6) },
+    { key = '8', mods = 'ALT', action = act.ActivateTab(7) },
+    { key = '9', mods = 'ALT', action = act.ActivateTab(-1) },
+}
+
 -- Returns the configuration to wezterm
 return config
