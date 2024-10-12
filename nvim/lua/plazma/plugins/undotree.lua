762a52e048fce848bcc7aeb941289154dff91d41
PLAZMAMA <maorioriori@gmail.com>
2024-10-12 14:28:02 -0400

    switch from 'FC' to 'diff'

--- nvim/lua/plazma/plugins/undotree.lua
+++ nvim/lua/plazma/plugins/undotree.lua
@@ -2,6 +2,6 @@ return {
     "mbbill/undotree",
     config = function()
         vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, {desc = "Opens undotree."})
-        vim.g.undotree_DiffCommand = "FC"
+        vim.g.undotree_DiffCommand = "diff"
     end,
 }
