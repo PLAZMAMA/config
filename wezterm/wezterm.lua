239cc9475a25bc9d9743f15caadf12c66f07d0f7
PLAZMAMA <maorioriori@gmail.com>
2024-10-12 19:21:47 -0400

    remove keybindings for tabs 5-10

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,19 +6,15 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'Tokyo Night Moon'
 config.window_background_opacity = 0.8
+config.disable_default_key_bindings = true
 
 local act = wezterm.action
 config.keys = {
-    { key = '1', mods = 'ALT', action = act.ActivateTab(0) },
-    { key = '2', mods = 'ALT', action = act.ActivateTab(1) },
-    { key = '3', mods = 'ALT', action = act.ActivateTab(2) },
-    { key = '4', mods = 'ALT', action = act.ActivateTab(3) },
-    { key = '5', mods = 'ALT', action = act.ActivateTab(4) },
-    { key = 'v', mods = 'ALT', action = act.SplitHorizontal{ domain = 'CurrentPaneDomain' } },
-    { key = '6', mods = 'ALT', action = act.ActivateTab(5) },
-    { key = '7', mods = 'ALT', action = act.ActivateTab(6) },
-    { key = '8', mods = 'ALT', action = act.ActivateTab(7) },
-    { key = '9', mods = 'ALT', action = act.ActivateTab(-1) },
+   { key = '1', mods = 'ALT', action = act.ActivateTab(0) },
+   { key = '2', mods = 'ALT', action = act.ActivateTab(1) },
+   { key = '3', mods = 'ALT', action = act.ActivateTab(2) },
+   { key = '4', mods = 'ALT', action = act.ActivateTab(3) },
+   { key = 'v', mods = 'ALT', action = act.SplitHorizontal{ domain = 'CurrentPaneDomain' } },
 }
 
 -- Returns the configuration to wezterm
