85a06149bfdc2883e3a8984071843626ed59f251
PLAZMAMA <maorioriori@gmail.com>
2024-10-12 20:44:49 -0400

    fix wezterm eating up C-{key}

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,7 +6,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'Tokyo Night Moon'
 config.window_background_opacity = 0.8
-config.disable_default_key_bindings = true
+config.disable_default_key_bindings = false
 
 local act = wezterm.action
 config.keys = {
@@ -17,6 +17,11 @@ config.keys = {
    { key = 'v', mods = 'ALT', action = act.SplitHorizontal{ domain = 'CurrentPaneDomain' } },
    { key = 'C', mods = 'CTRL|SHIFT', action = act.CopyTo('Clipboard') },
    { key = 'V', mods = 'CTRL|SHIFT', action = act.PasteFrom('Clipboard') },
+   { key = '7', mods = 'CTRL', action = wezterm.action.SendKey{key="7", mods="CTRL"} },
+   { key = '8', mods = 'CTRL', action = wezterm.action.SendKey{key="8", mods="CTRL"} },
+   { key = '9', mods = 'CTRL', action = wezterm.action.SendKey{key="9", mods="CTRL"} },
+   { key = '0', mods = 'CTRL', action = wezterm.action.SendKey{key="0", mods="CTRL"} },
+   { key = '/', mods = 'CTRL', action = wezterm.action.SendKey{key="/", mods="CTRL"} },
 }
 
 -- Returns the configuration to wezterm
