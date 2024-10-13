42f7a0fcf35bf304a925751e5d733b48e4b7c73b
PLAZMAMA <maorioriori@gmail.com>
2024-10-12 20:25:40 -0400

    upgrade from harpoon 1 to 2

--- nvim/lua/plazma/plugins/harpoon.lua
+++ nvim/lua/plazma/plugins/harpoon.lua
@@ -1,18 +1,20 @@
 return {
     "ThePrimeagen/harpoon",
+    version="",
+    branch="harpoon2",
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
     config = function()
         local keymap = vim.keymap
-        local mark = require("harpoon.mark")
-        local ui = require("harpoon.ui")
-        
-        keymap.set("n", "<leader>a", mark.add_file)
-        keymap.set("n", "<M-e>", ui.toggle_quick_menu)
-        keymap.set("n", "<M-7>", function() ui.nav_file(1) end)
-        keymap.set("n", "<M-8>", function() ui.nav_file(2) end)
-        keymap.set("n", "<M-9>", function() ui.nav_file(3) end)
-        keymap.set("n", "<M-0>", function() ui.nav_file(4) end)
+        local harpoon = require("harpoon")
+
+        harpoon:setup()
+        keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+        keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        keymap.set("n", "<C-7>", function() harpoon:list():select(1) end)
+        keymap.set("n", "<C-8>", function() harpoon:list():select(2) end)
+        keymap.set("n", "<C-9>", function() harpoon:list():select(1) end)
+        keymap.set("n", "<C-0>", function() harpoon:list():select(1) end)
     end,
 }
