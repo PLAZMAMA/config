9f30d122a7a1942f83e2ef803dc05db43975e7cc
PLAZMAMA <maorioriori@gmail.com>
2024-10-12 21:48:38 -0400

    move all relevant keybinding modes from ALT to CTRL

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,11 +10,11 @@ config.disable_default_key_bindings = false
 
 local act = wezterm.action
 config.keys = {
-   { key = '1', mods = 'ALT', action = act.ActivateTab(0) },
-   { key = '2', mods = 'ALT', action = act.ActivateTab(1) },
-   { key = '3', mods = 'ALT', action = act.ActivateTab(2) },
-   { key = '4', mods = 'ALT', action = act.ActivateTab(3) },
-   { key = 'v', mods = 'ALT', action = act.SplitHorizontal{ domain = 'CurrentPaneDomain' } },
+   { key = '1', mods = 'CTRL', action = act.ActivateTab(0) },
+   { key = '2', mods = 'CTRL', action = act.ActivateTab(1) },
+   { key = '3', mods = 'CTRL', action = act.ActivateTab(2) },
+   { key = '4', mods = 'CTRL', action = act.ActivateTab(3) },
+   { key = 'v', mods = 'CTRL', action = act.SplitHorizontal{ domain = 'CurrentPaneDomain' } },
    { key = 'C', mods = 'CTRL|SHIFT', action = act.CopyTo('Clipboard') },
    { key = 'V', mods = 'CTRL|SHIFT', action = act.PasteFrom('Clipboard') },
    { key = '7', mods = 'CTRL', action = wezterm.action.SendKey{key="7", mods="CTRL"} },
