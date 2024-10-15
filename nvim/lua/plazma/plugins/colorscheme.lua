9ede2559610aafa66e5985775367cdf1b1ef4f88
PLAZMAMA <maorioriori@gmail.com>
2024-10-14 21:00:59 -0400

    change fg_gutter color to a more visiable one

--- nvim/lua/plazma/plugins/colorscheme.lua
+++ nvim/lua/plazma/plugins/colorscheme.lua
@@ -7,12 +7,15 @@ return {
         priority = 1000,
         config = function()
             require("tokyonight").setup {
-                style = "moon",
+                style = "night",
                 transparent = true, -- Enable this to disable setting the background color
                 styles={
                     sidebars = "transparent",
                     floats = "transparent",
                 },
+                on_colors = function(colors)
+                    colors.fg_gutter = colors.blue0
+                end,
             }
             require("tokyonight").load()
         end,
