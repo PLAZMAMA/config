6fdb75658ee610bcbd3f13dfa669da7c91819e7b
PLAZMAMA <maorioriori@gmail.com>
2024-10-14 20:08:44 -0400

    add which-key docs to harpoon

--- nvim/lua/plazma/plugins/harpoon.lua
+++ nvim/lua/plazma/plugins/harpoon.lua
@@ -16,5 +16,19 @@ return {
         keymap.set('n', '<C-8>', function() harpoon:list():select(2) end)
         keymap.set('n', '<C-9>', function() harpoon:list():select(3) end)
         keymap.set('n', '<C-0>', function() harpoon:list():select(4) end)
+        -- document existing key chains
+        local wk = require('which-key')
+        wk.add({
+            {
+                mode = { 'n' },
+                {'<leader>a', desc = '[A]dd file to Harpoon'},
+                {'<C-e>', desc = 'Toggle Harpoon menue'},
+                {'<C-7>', desc = 'Go to Harpoon first file'},
+                {'<C-8>', desc = 'Go to Harpoon second file'},
+                {'<C-9>', desc = 'Go to Harpoon third file'},
+                {'<C-0>', desc = 'Go to Harpoon fourth file'},
+            },
+
+        })
     end,
 }
