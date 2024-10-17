06773355632f7590a2c5caaf73371377909c0f71
PLAZMAMA <maorioriori@gmail.com>
2024-10-16 21:25:04 -0400

    reactivate codeium

--- nvim/lua/plazma/plugins/codeium.lua
+++ nvim/lua/plazma/plugins/codeium.lua
@@ -1,30 +1,37 @@
-return {}
-
-
+-- return {}
 
 -----------------------------------------
 -- Enables codeium if the need arises to.
 
--- return {
---   'Exafunction/codeium.vim',
---   event = 'BufEnter',
---   config = function()
---     vim.g.codeium_disable_bindings = 1
---     vim.keymap.set('i', '<C-a>', function() return vim.fn['codeium#Accept']() end, { expr = true })
---     vim.keymap.set('i', '<M-]>', function() return vim.fn['codeium#CycleCompletions'](1) end,
---       { expr = true, silent = true })
---     vim.keymap.set('i', '<M-[>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
---       { expr = true, silent = true })
---     vim.keymap.set('i', '<M-c>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
---     vim.keymap.set('n', '<leader>mt', function() vim.g.codeium_enabled = not vim.g.codeium_enabled end,
---       { expr = true, silent = true })
---
---     local wk = require("which-key")
---     wk.add({
---       {
---         mode = { 'n' },
---         {'<leader>mt', desc = '[M]atrix(codeium) [T]oggle'},
---       },
---     })
---   end,
--- }
+return {
+    'Exafunction/codeium.vim',
+    event = 'BufEnter',
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "hrsh7th/nvim-cmp",
+    },
+    config = function()
+        -- require('codeium').setup({
+        --     enable_chat = true,
+        --     detect_proxy = true,
+        -- })
+
+        vim.g.codeium_disable_bindings = 1
+        vim.keymap.set('i', '<C-a>', function() return vim.fn['codeium#Accept']() end, { expr = true })
+        vim.keymap.set('i', '<M-l>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+            { expr = true, silent = true })
+        vim.keymap.set('i', '<M-h>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+            { expr = true, silent = true })
+        vim.keymap.set('i', '<M-c>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+        vim.keymap.set('n', '<leader>ct', function() vim.g.codeium_enabled = not vim.g.codeium_enabled end,
+            { expr = true, silent = true })
+
+        local wk = require("which-key")
+        wk.add({
+            {
+                mode = { 'n' },
+                { '<leader>ct', desc = '[C]odeium) [T]oggle' },
+            },
+        })
+    end,
+}
