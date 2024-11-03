9982804b43911b59357136d2f1ba0d1a4cd376b8
PLAZMAMA <maorioriori@gmail.com>
2024-11-03 03:28:22 -0500

    remove redundant plazma folder

--- nvim/lua/plazma/core/init.lua
+++
@@ -1,4 +0,0 @@
-require('plazma.core.options')
-require('plazma.core.keymaps')
-require('plazma.core.yank_highlight')
-require('plazma.core.neovide')
--- nvim/lua/plazma/plugins/init.lua
+++
@@ -1,5 +0,0 @@
-return {
-    'tpope/vim-sleuth',
-    'tpope/vim-rhubarb',
-    'tpope/vim-fugitive',
-}
