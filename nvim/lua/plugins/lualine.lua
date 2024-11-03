6e329e9d1900b0df40a9e46aa537467ddef47102
PLAZMAMA <maorioriori@gmail.com>
2024-11-03 15:10:30 -0500

    add document context to lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,14 +1,28 @@
 return {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    version = '*',
-    opts = {
-        options = {
-            icons_enabled = false,
-            theme = 'auto',
-            component_separators = '|',
-            section_separators = '',
+  'nvim-lualine/lualine.nvim',
+  config = function()
+    local trouble = require 'trouble'
+    local symbols = trouble.statusline {
+      mode = 'lsp_document_symbols',
+      groups = {},
+      title = false,
+      filter = { range = true },
+      -- format = '{kind_icon}{symbol.name:Normal}',
+      format = '{kind_icon}{symbol.name:Normal}',
+      -- The following line is needed to fix the background color
+      -- Set it to the lualine section you want to use
+      hl_group = 'lualine_c_normal',
+    }
+    require('lualine').setup {
+      sections = {
+        lualine_c = {
+          'filename',
+          {
+            symbols.get,
+            cond = symbols.has,
+          },
         },
-    },
+      },
+    }
+  end,
 }
