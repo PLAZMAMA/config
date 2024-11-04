38f1950770f93a4912287349ae76a393619455c6
PLAZMAMA <maorioriori@gmail.com>
2024-11-04 16:31:16 -0500

    add and update key mappings

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -24,12 +24,30 @@ return {
         completeopt = 'menu,menuone,noinsert',
       },
       mapping = cmp.mapping.preset.insert {
-        ['<A-Up>'] = cmp.mapping.scroll_docs(-4),
-        ['<A-Down>'] = cmp.mapping.scroll_docs(4),
+        ['<A-p>'] = cmp.mapping.scroll_docs(-4),
+        ['<A-n>'] = cmp.mapping.scroll_docs(4),
         ['<Tab>'] = cmp.mapping.confirm {
           behavior = cmp.ConfirmBehavior.Replace,
           select = true,
         },
+        ['<C-n>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.locally_jumpable(1) then
+            luasnip.jump(1)
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+        ['<C-p>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
       },
       sources = {
         { name = 'nvim_lsp' },
