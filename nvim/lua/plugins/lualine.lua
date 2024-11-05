8ffe706d3436341a8ef24b1db98aae835aeba5f8
PLAZMAMA <maorioriori@gmail.com>
2024-11-04 19:07:27 -0500

    add harpoon mark indicator to lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,5 +1,41 @@
+---returns a harpoon icon if the current file is marked in Harpoon. Does not
+---`require` Harpoon itself (allowing harpoon to still be lazy-loaded)
+local function updateHarpoonIndicator()
+  local harpoon = require 'harpoon'
+  vim.b.harpoonMark = '' -- empty by default
+  local pwd = vim.uv.cwd()
+  if not pwd then
+    return
+  end
+  local marked_files = harpoon:list():display()
+  local current_file = vim.fn.expand '%:p'
+  for _, file in pairs(marked_files) do
+    local abs_path = pwd .. '/' .. file
+    if abs_path == current_file then
+      vim.b.harpoonMark = '󰛢'
+    end
+  end
+end
+
+local function harpoonStatusline()
+  return vim.b.harpoonMark or ''
+end
+
+-- Updates the harpoon indicator when writing in the harpoon window or in general. This time not override the whole write command :)
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost' }, {
+  pattern = '*',
+  callback = updateHarpoonIndicator,
+})
+
+-- Updates the harpoon indicator when adding files to harpoon.
+vim.api.nvim_create_autocmd({ 'User' }, {
+  pattern = 'HarpoonAdd',
+  callback = updateHarpoonIndicator,
+})
+
 return {
   'nvim-lualine/lualine.nvim',
+  dependencies = { 'folke/trouble.nvim', 'ThePrimeagen/harpoon' },
   config = function()
     local trouble = require 'trouble'
     local symbols = trouble.statusline {
@@ -22,6 +58,7 @@ return {
             cond = symbols.has,
           },
         },
+        lualine_x = { harpoonStatusline },
       },
     }
   end,
