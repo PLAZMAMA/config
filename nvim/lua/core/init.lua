ac41e7cac2ff22f0f96957b9bce1071c87617bd3
PLAZMAMA <maorioriori@gmail.com>
2024-12-15 17:58:00 -0500

    add core.commands and change file to new format

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -1,3 +1,4 @@
-require('core.options')
-require('core.keymaps')
-require('core.yank_highlight')
+require("core.options")
+require("core.keymaps")
+require("core.yank_highlight")
+require("core.commands")
