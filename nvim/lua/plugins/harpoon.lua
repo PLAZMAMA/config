b7229004d5f88b9d6214681a695f72b89b802e5c
PLAZMAMA <maorioriori@gmail.com>
2024-12-16 16:11:16 -0500

    move whichkey docs to inside keymap.set and add insert mode to keybindings

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -10,37 +10,24 @@ return {
         local harpoon = require("harpoon")
 
         harpoon:setup()
-        keymap.set("n", "<leader>a", function()
+        keymap.set({"n", "i"}, "<leader>a", function()
             harpoon:list():add()
             vim.api.nvim_exec_autocmds("User", { pattern = "HarpoonAdd" })
-        end)
-        keymap.set("n", "<C-e>", function()
+        end, { desc = "[A]dd file to Harpoon"})
+        keymap.set({"n", "i"}, "<C-e>", function()
             harpoon.ui:toggle_quick_menu(harpoon:list())
-        end)
-        keymap.set("n", "<C-7>", function()
+        end, {desc = "Toggle Harpoon menue"})
+        keymap.set({"n", "i"}, "<C-7>", function()
             harpoon:list():select(1)
-        end)
-        keymap.set("n", "<C-8>", function()
+        end, {desc = "Go to Harpoon first file"})
+        keymap.set({"n", "i"}, "<C-8>", function()
             harpoon:list():select(2)
-        end)
-        keymap.set("n", "<C-9>", function()
+        end, {desc = "Go to Harpoon second file"})
+        keymap.set({"n", "i"}, "<C-9>", function()
             harpoon:list():select(3)
-        end)
-        keymap.set("n", "<C-0>", function()
+        end, {desc = "Go to Harpoon third file"})
+        keymap.set({"n", "i"}, "<C-0>", function()
             harpoon:list():select(4)
-        end)
-        -- document existing key chains
-        local wk = require("which-key")
-        wk.add({
-            {
-                mode = { "n" },
-                { "<leader>a", desc = "[A]dd file to Harpoon" },
-                { "<C-e>", desc = "Toggle Harpoon menue" },
-                { "<C-7>", desc = "Go to Harpoon first file" },
-                { "<C-8>", desc = "Go to Harpoon second file" },
-                { "<C-9>", desc = "Go to Harpoon third file" },
-                { "<C-0>", desc = "Go to Harpoon fourth file" },
-            },
-        })
+        end, {desc = "Go to Harpoon fourth file"})
     end,
 }
