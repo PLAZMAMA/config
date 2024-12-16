8bdeb8c2b1f56f46e094a2857d9c5465b627d6fc
PLAZMAMA <maorioriori@gmail.com>
2024-12-15 21:41:50 -0500

    remove [/] Fuzzily search in current buffer keymap

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -91,16 +91,6 @@ return {
             require("telescope.builtin").buffers,
             { desc = "[ ] Find existing buffers" }
         )
-        vim.keymap.set("n", "<leader>/", function()
-            -- You can pass additional configuration to telescope to change theme, layout, etc.
-            require("telescope.builtin").current_buffer_fuzzy_find(
-                require("telescope.themes").get_dropdown({
-                    winblend = 10,
-                    previewer = false,
-                })
-            )
-        end, { desc = "[/] Fuzzily search in current buffer" })
-
         local function telescope_live_grep_open_files()
             require("telescope.builtin").live_grep({
                 grep_open_files = true,
