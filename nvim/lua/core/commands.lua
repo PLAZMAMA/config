830f281e2e3dbc78cf4e299d193513e3e5910ada
PLAZMAMA <maorioriori@gmail.com>
2024-12-15 22:02:57 -0500

    add git commit ammend user command

--- nvim/lua/core/commands.lua
+++ nvim/lua/core/commands.lua
@@ -5,7 +5,7 @@
 vim.api.nvim_create_user_command("E", "Explore", {})
 vim.api.nvim_create_user_command("GC", "Git commit % -v", {})
 vim.api.nvim_create_user_command("GCA", "Git commit -va", {})
-vim.api.nvim_create_user_command("GA", "Git commit --amend", {})
+vim.api.nvim_create_user_command("GA", "Git commit -v --amend", {})
 vim.api.nvim_create_user_command("GP", "Git push", {})
 vim.api.nvim_create_user_command("GPL", "Git pull", {})
 vim.api.nvim_create_user_command("GL", "Git log", {})
