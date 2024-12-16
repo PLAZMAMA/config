c4fe9a42be2a407694a9cbd897d99d562bdb302e
PLAZMAMA <maorioriori@gmail.com>
2024-12-16 02:05:12 -0500

    add harpoon market files to lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,5 +1,5 @@
----returns a harpoon icon if the current file is marked in Harpoon. Does not
----`require` Harpoon itself (allowing harpoon to still be lazy-loaded)
+-- returns a harpoon icon if the current file is marked in Harpoon. Does not
+-- `require` Harpoon itself (allowing harpoon to still be lazy-loaded)
 local function update_harpoon()
     -- Get current market files
     local harpoon = require("harpoon")
@@ -9,22 +9,43 @@ local function update_harpoon()
     end
 
     -- Update harpoon statusline (marks and files)
-    vim.b.harpoonMark = "" -- empty by default
+    vim.b.harpoon_mark = "" -- empty by default
     local marked_files = harpoon:list():display()
     local current_file = vim.api.nvim_buf_get_name(0)
+    local marked_file_names = {}
     for _, file in pairs(marked_files) do
         local abs_path = file
         if string.sub(abs_path, 1, 1) ~= "/" then
             abs_path = pwd .. "/" .. abs_path
         end
         if abs_path == current_file then
-            vim.b.harpoonMark = "󰛢"
+            vim.b.harpoon_mark = "󰛢"
         end
+        table.insert(marked_file_names, vim.fs.basename(file))
     end
+    vim.g.marked_file_names = marked_file_names
 end
 
 local function harpoon_mark()
-    return vim.b.harpoonMark or ""
+    return vim.b.harpoon_mark or ""
+end
+
+local function harpoon_files()
+    local marked_files = ""
+    -- Harpoon's corresponding CTRL number starts at 7.
+    -- Hence for the first index, it will be 6 + 1 = 7.
+    -- For the second it will be 6 + 2 = 8.
+    -- and so on...
+    local start_indx = 6
+    for key, value in pairs(vim.g.marked_file_names) do
+        marked_files = marked_files .. key + start_indx .. ":" .. value .. " "
+    end
+    -- Center files in statusline
+    local progress_len = 3 -- Complete guess
+    local lualine_padding = 14 -- Through trial and error
+    local used_space = vim.o.columns - (#marked_files + #vim.b.harpoon_mark + progress_len + lualine_padding)
+    local padding = string.rep(" ", used_space/2, "")
+    return (marked_files .. padding) or ""
 end
 
 -- Updates the harpoon indicator when writing in the harpoon window or in general. This time not override the whole write command :)
@@ -48,7 +69,9 @@ return {
     config = function()
         require("lualine").setup({
             sections = {
-                lualine_x = {harpoon_mark},
+                lualine_x = { harpoon_files },
+                lualine_y = { harpoon_mark },
+                lualine_z = { "progress" },
             },
         })
     end,
