0eac672f022aa0b736cd4b8acb95e74c483d4626
PLAZMAMA <maorioriori@gmail.com>
2024-12-15 19:08:44 -0500

    disable undotree diff panel

--- nvim/lua/plugins/undotree.lua
+++ nvim/lua/plugins/undotree.lua
@@ -8,11 +8,12 @@ return {
             vim.cmd.UndotreeToggle,
             { desc = "Opens undotree." }
         )
+        vim.g.undotree_DiffpanelHeight = 0
+        vim.g.undotree_DiffAutoOpen = 0
         if string.match(vim.uv.os_uname().sysname, "Windows") then
             vim.g.undotree_DiffCommand = "FC"
         else
             vim.g.undotree_DiffCommand = "diff"
         end
-        -- g:UndootreeDiffpanelHeight = 0
     end,
 }
