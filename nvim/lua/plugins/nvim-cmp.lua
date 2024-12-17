5f04df6248d95b9eb7edece3336b482b4963901b
PLAZMAMA <maorioriori@gmail.com>
2024-12-16 21:36:28 -0500

    lazy load nvim-cmp

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -2,6 +2,7 @@ return {
     -- Autocompletion
     "hrsh7th/nvim-cmp",
     commit = "ae644feb7b67bf1ce4260c231d1d4300b19c6f30",
+    event = "InsertEnter",
     dependencies = {
         -- Adds LSP completion capabilities
         "hrsh7th/cmp-nvim-lsp",
