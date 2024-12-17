29ca817230558d39b3b49dcf86f936a3e53828e8
PLAZMAMA <maorioriori@gmail.com>
2024-12-16 21:43:14 -0500

    early return to avoid over nesting and elaborate on it

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -53,46 +53,50 @@ return {
                 map("K", vim.lsp.buf.hover, "Hover Documentation")
                 map("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
 
+                -- Makes sure the lsp has the highlight feature needed for the two autocommands below.
+                local client = vim.lsp.get_client_by_id(event.data.client_id)
+                if
+                    not (
+                        client
+                        and client.supports_method(
+                            vim.lsp.protocol.Methods.textDocument_documentHighlight
+                        )
+                    )
+                then
+                    return
+                end
                 -- The following two autocommands are used to highlight references of the
                 -- word under your cursor when your cursor rests there for a little while.
                 --    See `:help CursorHold` for information about when this is executed
                 --
                 -- When you move your cursor, the highlights will be cleared (the second autocommand).
-                local client = vim.lsp.get_client_by_id(event.data.client_id)
-                if
-                    client
-                    and client.supports_method(
-                        vim.lsp.protocol.Methods.textDocument_documentHighlight
-                    )
-                then
-                    local highlight_augroup =
-                        vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
-                    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-                        buffer = event.buf,
-                        group = highlight_augroup,
-                        callback = vim.lsp.buf.document_highlight,
-                    })
+                local highlight_augroup =
+                    vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
+                vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+                    buffer = event.buf,
+                    group = highlight_augroup,
+                    callback = vim.lsp.buf.document_highlight,
+                })
 
-                    vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-                        buffer = event.buf,
-                        group = highlight_augroup,
-                        callback = vim.lsp.buf.clear_references,
-                    })
+                vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+                    buffer = event.buf,
+                    group = highlight_augroup,
+                    callback = vim.lsp.buf.clear_references,
+                })
 
-                    vim.api.nvim_create_autocmd("LspDetach", {
-                        group = vim.api.nvim_create_augroup(
-                            "lsp-detach",
-                            { clear = true }
-                        ),
-                        callback = function(event2)
-                            vim.lsp.buf.clear_references()
-                            vim.api.nvim_clear_autocmds({
-                                group = "lsp-highlight",
-                                buffer = event2.buf,
-                            })
-                        end,
-                    })
-                end
+                vim.api.nvim_create_autocmd("LspDetach", {
+                    group = vim.api.nvim_create_augroup(
+                        "lsp-detach",
+                        { clear = true }
+                    ),
+                    callback = function(event2)
+                        vim.lsp.buf.clear_references()
+                        vim.api.nvim_clear_autocmds({
+                            group = "lsp-highlight",
+                            buffer = event2.buf,
+                        })
+                    end,
+                })
             end,
         })
     end,
