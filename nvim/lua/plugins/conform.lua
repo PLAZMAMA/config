0acd1424a2c078ae8dd20f2e0cfd832f06f5e2f3
PLAZMAMA <maorioriori@gmail.com>
2024-12-17 19:18:45 -0500

    fix python formatting setting

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -17,7 +17,7 @@ return {
         notify_on_error = false,
         formatters_by_ft = {
             lua = { "stylua" },
-            python = { "ruff" },
+            python = {"ruff_organize_imports", "ruff_format" },
             -- You can use 'stop_after_first' to run the first available formatter from the list
             javascript = { "prettierd" },
             html = { "prettierd" },
