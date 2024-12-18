3c044cb99b04b8b9d99341d88df53b15b5148a78
PLAZMAMA <maorioriori@gmail.com>
2024-12-18 15:09:12 -0500

    add blink.cmp plugin

---
+++ nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,25 @@
+return {
+    "saghen/blink.cmp",
+    dependencies = "rafamadriz/friendly-snippets",
+    version = "v0.*",
+    opts = {
+        keymap = { preset = "default" },
+
+        appearance = {
+            use_nvim_cmp_as_default = true,
+            nerd_font_variant = "normal",
+        },
+
+        -- default list of enabled providers defined so that you can extend it
+        -- elsewhere in your config, without redefining it, via `opts_extend`
+        sources = {
+            default = { "lsp", "path", "snippets", "buffer" },
+        },
+
+        -- experimental signature help support
+        signature = { enabled = true }
+    },
+    -- allows extending the providers array elsewhere in your config
+    -- without having to redefine it
+    opts_extend = { "sources.default" },
+}
