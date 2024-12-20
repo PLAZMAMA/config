2bc938c16f7b8acf3b11ce5633797b5b8a139065
PLAZMAMA <maorioriori@gmail.com>
2024-12-19 20:31:51 -0500

    add copilot.lua plugin

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,39 @@
+return {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    keys = {
+        {
+            "<C-a><C-a>",
+            function()
+                require("copilot.suggestion").accept()
+            end,
+            mode = "i",
+            desc = "Accept copilot suggestion",
+        },
+        {
+            "<C-a><C-w>",
+            function()
+                require("copilot.suggestion").accept_word()
+            end,
+            mode = "i",
+            desc = "Accept the first word of copilot suggestion",
+        },
+        {
+            "<C-a><C-l>",
+            function()
+                require("copilot.suggestion").accept_line()
+            end,
+            mode = "i",
+            desc = "Accept the first line of copilot suggestion",
+        },
+        {
+            "<C-h>",
+            function()
+                require("copilot.suggestion").dismiss()
+            end,
+            mode = "i",
+            desc = "Dismiss copilot suggestion",
+        },
+    },
+    opts = { suggestion = { auto_trigger = true } },
+}
