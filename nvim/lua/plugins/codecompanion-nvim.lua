cd079e7aee41da29293908cd0d033a357a477501
PLAZMAMA <maorioriori@gmail.com>
2024-12-19 21:15:07 -0500

    enable codecompanion and switch to copilot adapter

--- nvim/lua/plugins/codecompanion-nvim.lua
+++ nvim/lua/plugins/codecompanion-nvim.lua
@@ -1,74 +1,89 @@
--- remove plugin for now since I can't make the remote OpenAI compatible providers work
--- TODO: Make OpenAI compatible providers work
 -- TODO: Add custom prompts for function definition generation in different languages. (partly coming up in the next version hopefully)
-return {}
+return {
+    "olimorris/codecompanion.nvim",
+    enabled = true,
+    version = "*",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "MeanderingProgrammer/render-markdown.nvim",
+        "zbirenbaum/copilot.lua", -- Optional: For using slash commands and variables in the chat buffer
+        "saghen/blink.cmp",
+        "nvim-telescope/telescope.nvim", -- Optional: For using slash commands
+        { "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves `vim.ui.select`
+    },
+    keys = {
+        {
+            "<leader>sa",
+            "<cmd>CodeCompanionActions<cr>",
+            noremap = true,
+            silent = true,
+            desc = "[S]earch Code Companion [A]ctions",
+        },
+        {
+            "<leader>ct",
+            "<cmd>CodeCompanionChat Toggle<cr>",
+            noremap = true,
+            desc = "Code Companion [C]hat [T]oggle",
+        },
+        {
+            "<leader>ct",
+            "<cmd>CodeCompanionChat<cr>",
+            mode = "v",
+            noremap = true,
+            desc = "Code Companion [C]hat [T]oggle",
+        },
+        {
+            "<leader>ce",
+            "<cmd>CodeCompanion /explain<cr>",
+            mode = "v",
+            noremap = true,
+            silent = true,
+            desc = "[C]ode Companion [E]xplain",
+        },
+        {
+            "<leader>cc",
+            "<cmd>CodeCompanion<cr>",
+            mode = { "n", "v" },
+            noremap = true,
+        },
+    },
+    opts = {
+        send_code = true,
+        log_level = "DEBUG",
+        display = {
+            chat = {
+                render_headers = true,
+            },
+        },
+        strategies = {
+            chat = {
+                adapter = "copilot",
+            },
+            inline = {
+                adapter = "copilot",
+            },
+            agent = {
+                adapter = "copilot",
+            },
+        },
+        adapters = {
+            copilot = function()
+                return require("codecompanion.adapters").extend("copilot", {
+                    model = {
+                        default = "claude-3.5-sonnet",
+                    }
+                })
+            end,
+        },
+    },
+}
 
--- return {
---   'olimorris/codecompanion.nvim',
---   version = '9.x.x',
---   dependencies = {
---     'nvim-lua/plenary.nvim',
---     'nvim-treesitter/nvim-treesitter',
---     'MeanderingProgrammer/render-markdown.nvim',
---     'hrsh7th/nvim-cmp', -- Optional: For using slash commands and variables in the chat buffer
---     'nvim-telescope/telescope.nvim', -- Optional: For using slash commands
---     { 'stevearc/dressing.nvim', opts = {} }, -- Optional: Improves `vim.ui.select`
---   },
---   opts = {
---     send_code = true,
---     log_level = 'DEBUG',
---   },
---   config = function()
---     require('codecompanion').setup {
---       display = {
---         chat = {
---           render_headers = false,
---         },
---       },
---       adapters = {
---         ollama = function()
---           return require('codecompanion.adapters').extend('ollama', {
---             schema = {
---               model = {
---                 default = 'qwen2.5-coder',
---               },
---             },
---             env = {
---               url = 'http://192.168.1.30:8080',
---               -- url = 'http://localhost:11434',
---               api_key = 'OLLAMA_API_KEY',
---             },
---             headers = {
---               ['Content-Type'] = 'application/json',
---               ['Authorization'] = 'Bearer ${api_key}',
---             },
---             parameters = {
---               sync = true,
---             },
---           })
---         end,
---       },
---     }
---
---     -- Set provider aka ollama
---     vim.g.codecompanion_adapter = 'ollama'
---
---     -- Set keybinding
---     vim.api.nvim_set_keymap('n', '<leader>cac', '<cmd>CodeCompanionActions<cr>', { noremap = true, silent = true })
---     vim.api.nvim_set_keymap('v', '<leader>cac', '<cmd>CodeCompanionActions<cr>', { noremap = true, silent = true })
---     vim.api.nvim_set_keymap('n', '<leader>ct', '<cmd>CodeCompanionChat Toggle<cr>', { noremap = true })
---     vim.api.nvim_set_keymap('v', '<leader>ct', '<cmd>CodeCompanionChat<cr>', { noremap = true })
---     vim.api.nvim_set_keymap('v', '<leader>ce', '<cmd>CodeCompanion /explain<cr>', { noremap = true, silent = true })
---     vim.api.nvim_set_keymap('n', '<leader>cc', '<cmd>CodeCompanion<cr>', { noremap = true })
---     vim.api.nvim_set_keymap('v', '<leader>cc', '<cmd>CodeCompanion<cr>', { noremap = true })
---   end,
--- }
-
-----------------------------------------------------------------------------
+--------------------------------------------------------------------------
 -- Doesn't work yet
 -- Keybinding for the prompt in prompt_library that doesn't work.
 -- vim.api.nvim_set_keymap('v', '<leader>cn', '<cmd>CodeCompanion /naming<cr>', { noremap = true, silent = true })
---
+
 -- prompt_library = {
 --   -- Custom promps/behaviors (ex. /{custom_behavior_name})
 --   ['Naming'] = {
@@ -98,37 +113,4 @@ return {}
 --       },
 --     },
 --   },
--- },
---
--- glhf = function()
---   return require('codecompanion.adapters').extend('openai_compatible', {
---     env_replaced = {
---       url = 'https://glhf.chat/api/openai',
---       api_key = 'GLHF_API_KEY',
---     },
---   })
--- end,
--- ----------------------------------------------------------------------------
---
---
--- ollama = function()
---   return require('codecompanion.adapters').extend('ollama', {
---     schema = {
---       model = {
---         default = 'qwen2.5-coder',
---       },
---     },
---     env = {
---       -- url = 'http://192.168.1.30:8080',
---       url = 'http://localhost:11434',
---       api_key = 'OLLAMA_API_KEY',
---     },
---     headers = {
---       ['Content-Type'] = 'application/json',
---       ['Authorization'] = 'Bearer ${api_key}',
---     },
---     parameters = {
---       sync = true,
---     },
---   })
--- end,
+-- }
