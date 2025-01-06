3443c6b11bbb53127f3f13badb437cd9f338f021
PLAZMAMA <maorioriori@gmail.com>
2025-01-05 19:41:13 -0500

    add ruff and additional lua LS options

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -33,7 +33,22 @@ return {
                         callSnippet = "Replace",
                         telemetry = { enable = false },
                         -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-                        diagnostics = { disable = { "missing-fields" } },
+                        diagnostics = {
+                            disable = {
+                                "missing-fields",
+                                "unused-local",
+                                "unused-function",
+                            },
+                        },
+                    },
+                },
+            },
+            ruff = {
+                init_options = {
+                    settings = {
+                        lint = {
+                            enable = false
+                        }
                     },
                 },
             },
