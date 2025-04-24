85b57b541b273865710f2bb8bce61332999f2648
PLAZMAMA <maorioriori@gmail.com>
2025-04-24 16:03:41 -0400

    remove vimwiki and replace with kiwi

---
+++ nvim/lua/plugins/kiwi.lua
@@ -0,0 +1,18 @@
+return {
+    "serenevoid/kiwi.nvim",
+    lazy = true,
+    opts = {
+        {
+            name = "wiki",
+            path = "~/Documents/plazmama.github.io/",
+        },
+    },
+    keys = {
+        {
+            "<leader>ww",
+            ':lua require("kiwi").open_wiki_index()<cr>',
+            desc = "Open Wiki index",
+        },
+        { "T", ':lua require("kiwi").todo.toggle()<cr>', desc = "Toggle Markdown Task" },
+    },
+}
